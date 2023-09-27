af2c8ef2dafc452916a3ac1ce4f08d328d202dad
Rashel <hcrashel@gmail.com>
2023-09-27 17:14:42 -0600

    [ADD] geocaching1:Creacion del proyecto ionic V.7

---
+++ geocaching1/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'geocaching1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
